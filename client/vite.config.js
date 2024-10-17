2d7b55d086c74b184b73cff8baf721c8849b6513
melisaruiz <144286838+MelisaRuiz@users.noreply.github.com>
2024-10-17 19:13:52 -0300

    Ajuste de la ruta base en vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Hecta.github.io/', // Asegúrate de que esta ruta coincida con el nombre de tu repositorio
+  base: '/',  // Asegúrate de que esta ruta coincida con el nombre de tu repositorio
 })
